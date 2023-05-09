bb8abf14480d2f995cb87db47f19ba06f258fd38
Bogdan <thegarrybit@gmail.com>
2023-05-10 00:25:03 +0300

    fix: copy folder to remove deleted files from target folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,21 +5,23 @@ const fs = require('fs');
 const TARGET_FOLDER_PATH = join(__dirname, 'files');
 const COPY_DIR_PATH = join(__dirname, 'files-copy');
 
-const createDirectory = async () => {
-  return await mkdir(COPY_DIR_PATH, { recursive: true });
+const createDirectory = () => {
+  return mkdir(COPY_DIR_PATH, { recursive: true });
 };
 
-createDirectory()
-  .then(
-    () => {
-      fs.readdir(TARGET_FOLDER_PATH, (err, files) => {
-        if (err) console.log(err);
+fs.rm(COPY_DIR_PATH, { recursive: true }, () => {
+  createDirectory()
+    .then(
+      () => {
+        fs.readdir(TARGET_FOLDER_PATH, (err, files) => {
+          if (err) console.log(err);
 
-        files.forEach(file => {
-          const filePath = join(TARGET_FOLDER_PATH, file);
-          const destinationPath = join(COPY_DIR_PATH, file);
+          files.forEach(file => {
+            const filePath = join(TARGET_FOLDER_PATH, file);
+            const destinationPath = join(COPY_DIR_PATH, file);
 
-          copyFile(filePath, destinationPath);
+            copyFile(filePath, destinationPath);
+          });
         });
       });
-    });
+});
